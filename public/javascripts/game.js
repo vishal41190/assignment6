eddd83766863cbe281978133dd30cba428e8c355
vishal <vishal41190@gmail.com>
2015-03-23 14:17:13 -0700

    Changed structure

---
+++ public/javascripts/game.js
@@ -0,0 +1,188 @@
+var main = function () {
+    "use strict";
+    var $button= $("#btnrock");   
+    $button.on("click",function(){
+    
+        
+    $.ajax({
+            
+            type: "GET",
+            url: "/play/rock",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+          
+        })
+        .done(function (data,status){
+        
+            $('#list').html("");
+            
+            $('#list').append("<div class='results'>");
+                    $('#list').append("<P>Outcome:"+data.outcome+"</p>");
+                    $('#list').append("<p>Wins:"+data.wins+"</p>");
+                    $('#list').append("<p>Looses:"+data.losses+"</p>");
+                    $('#list').append("<p>Ties:"+data.ties+"</p>");
+                    $('#list').append("</div>");
+               
+        })
+        .fail(function (data, status){
+            console.log("fail called");
+            console.log(data);
+            console.log(status);
+        });
+    
+      
+        
+    });
+
+//sandeep
+//1
+var $button= $("#btnpaper");   
+    $button.on("click",function(){
+    
+        
+    $.ajax({
+            
+            type: "GET",
+            url: "/play/paper",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+          
+        })
+        .done(function (data,status){
+        
+            $('#list').html("");
+            
+          $('#list').append("<div class='results'>");
+                    $('#list').append("<P>Outcome:"+data.outcome+"</p>");
+                    $('#list').append("<p>Wins:"+data.wins+"</p>");
+                    $('#list').append("<p>Looses:"+data.losses+"</p>");
+                    $('#list').append("<p>Ties:"+data.ties+"</p>");
+                    $('#list').append("</div>");
+               
+        })
+        .fail(function (data, status){
+            console.log("fail called");
+            console.log(data);
+            console.log(status);
+        });
+    
+      
+        
+    });
+
+//2
+
+
+
+var $button= $("#btnscissors");   
+    $button.on("click",function(){
+    
+        
+    $.ajax({
+            
+            type: "GET",
+            url: "/play/scissors",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+          
+        })
+        .done(function (data,status){
+        
+            $('#list').html("");
+            
+            $('#list').append("<div class='results'>");
+                    $('#list').append("<P>Outcome:"+data.outcome+"</p>");
+                    $('#list').append("<p>Wins:"+data.wins+"</p>");
+                    $('#list').append("<p>Looses:"+data.losses+"</p>");
+                    $('#list').append("<p>Ties:"+data.ties+"</p>");
+                    $('#list').append("</div>");
+               
+        })
+        .fail(function (data, status){
+            console.log("fail called");
+            console.log(data);
+            console.log(status);
+        });
+    
+      
+        
+    });
+
+
+//4
+
+var $button= $("#btnlizard");   
+    $button.on("click",function(){
+    
+        
+    $.ajax({
+            
+            type: "GET",
+            url: "/play/lizard",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+          
+        })
+        .done(function (data,status){
+        
+            $('#list').html("");
+            
+            $('#list').append("<div class='results'>");
+                    $('#list').append("<P>Outcome:"+data.outcome+"</p>");
+                    $('#list').append("<p>Wins:"+data.wins+"</p>");
+                    $('#list').append("<p>Looses:"+data.losses+"</p>");
+                    $('#list').append("<p>Ties:"+data.ties+"</p>");
+                    $('#list').append("</div>");
+               
+        })
+        .fail(function (data, status){
+            console.log("fail called");
+            console.log(data);
+            console.log(status);
+        });
+    
+      
+        
+    });
+
+
+//5
+
+var $button= $("#btnspock");   
+    $button.on("click",function(){
+    
+        
+    $.ajax({
+            
+            type: "GET",
+            url: "/play/spock",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+          
+        })
+        .done(function (data,status){
+        
+            $('#list').html("");
+            
+           $('#list').append("<div class='results'>");
+                    $('#list').append("<P>Outcome:"+data.outcome+"</p>");
+                    $('#list').append("<p>Wins:"+data.wins+"</p>");
+                    $('#list').append("<p>Looses:"+data.losses+"</p>");
+                    $('#list').append("<p>Ties:"+data.ties+"</p>");
+                    $('#list').append("</div>");
+               
+        })
+        .fail(function (data, status){
+            console.log("fail called");
+            console.log(data);
+            console.log(status);
+        });
+    
+      
+        
+    });
+//sandeep
+};
+
+$(document).ready(main);
+
---
+++ routes/play.js
@@ -0,0 +1,176 @@
+var express = require("express"),
+    http = require("http"),
+    router = express.Router();
+    
+var weapon = [
+            "scissors",
+             "paper",
+            "rock",
+             "lizard",
+             "spock"]
+
+
+var outcome;
+var wins=0;
+var losses=0;
+var ties=0;
+var random;
+var randomAns;
+// Create our Express-powered HTTP server
+// and have it listen on port 3000
+
+
+
+//http://stackoverflow.com/questions/1527803/generating-random-numbers-in-javascript-in-a-specific-range
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+// set up our routes
+// for user login
+router.get("/rock", function (req, res) {
+
+   random = getRandomInt(0,4);       
+        randomAns =   weapon[random];
+        switch(randomAns){
+                case "rock":
+                outcome="tie";
+                ties++;
+                break;
+                case "paper":
+                outcome="lose";
+                losses++;
+                break;
+                case "scissors":
+                outcome="win";
+                wins++;
+                break;
+                case "lizard":
+                outcome="win";
+                wins++;
+                break;
+                case "spock":
+                outcome="lose";
+                losses++;
+                break;
+        }
+     res.json({outcome:outcome, wins:wins,losses:losses,ties:ties});
+    });
+
+router.get("/paper",function(req,res){
+
+random = getRandomInt(0,4);       
+        randomAns =   weapon[random];
+        switch(randomAns){
+                case "rock":
+                outcome="win";
+                wins++;
+                break;
+                case "paper":
+                outcome="tie";
+                ties++;
+                break;
+                case "scissors":
+                outcome="lose";
+                losses++;
+                break;
+                case "lizard":
+                outcome="lose";
+                losses++;
+                break;
+                case "spock":
+                outcome="win";
+                wins++;
+                break;
+        }
+        res.json({outcome:outcome, wins:wins,losses:losses,ties:ties});
+});
+    
+router.get("/scissors",function(req,res){
+    
+     random = getRandomInt(0,4);       
+        randomAns =   weapon[random];
+        switch(randomAns){
+                case "rock":
+                outcome="lose";
+                losses++;
+                break;
+                case "paper":
+                outcome="win";
+                wins++;
+                break;
+                case "scissors":
+                outcome="tie";
+                ties++;
+                break;
+                case "lizard":
+                outcome="win";
+                wins++;
+                break;
+                case "spock":
+                outcome="lose";
+                losses++;
+                break;
+        }
+         res.json({outcome:outcome, wins:wins,losses:losses,ties:ties});
+});
+
+router.get('/lizard',function(req,res){
+    random = getRandomInt(0,4);       
+        randomAns =   weapon[random];
+        switch(randomAns){
+                case "rock":
+                outcome="lose";
+                losses++;
+                break;
+                case "paper":
+                outcome="win";
+                wins++;
+                break;
+                case "scissors":
+                outcome="lose";
+                losses++;
+                break;
+                case "lizard":
+                outcome="tie";
+                ties++;
+                break;
+                case "spock":
+                outcome="win";
+                wins++;
+                break;
+        }
+        res.json({outcome:outcome, wins:wins,losses:losses,ties:ties});
+});
+
+router.get('/spock',function(req,res){
+     random = getRandomInt(0,4);       
+        randomAns =   weapon[random];
+        switch(randomAns){
+                case "rock":
+                outcome="win";
+                wins++;
+                break;
+                case "paper":
+                outcome="lose";
+                losses++;
+                break;
+                case "scissors":
+                outcome="win";
+                wins++;
+                break;
+                case "lizard":
+                outcome="lose";
+                losses++;
+                break;
+                case "spock":
+                outcome="tie"
+                ties++;
+                break;
+        }
+         res.json({outcome:outcome, wins:wins,losses:losses,ties:ties});
+});
+
+module.exports = router;
